58e3f3afed354fa9c639e0386c17d731b8332986
Volodymyr Ponomarenko <volodymyr.ponomarenko@extrawest.com>
2020-02-12 17:00:18 +0200

    Generics

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,23 @@ const errorBag: ErrorContainer = {
   1: 'string',
 };
 
+const names: Array<string> = [];
+
+names.toString();
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It\'s success!!');
+  }, 2000)
+});
+
+console.log(promise);
+
+function merge<T extends object, U extends object>(objec_1: T, object_2: U) {
+  return Object.assign(object_2, objec_1);
+};
+
+const mergedObject = merge({name: 'Vlad'}, {age: 8});
+
+console.log(mergedObject);
 
